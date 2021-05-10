c7afa477fef833160c5934c6d5053a89a3b771bc
Gabor Dorcsinecz <gabor.dorcsinecz@gmail.com>
2021-05-10 14:44:52 +0100

    [CDSR-694][GD] NIP 381 | SC6a Select Basis for Claim

--- app/uk/gov/hmrc/cdsreimbursementclaim/models/eis/claim/enums/BasisOfClaim.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaim/models/eis/claim/enums/BasisOfClaim.scala
@@ -36,6 +36,9 @@ object BasisOfClaim {
   case object Preference extends BasisOfClaim
   case object RGR extends BasisOfClaim
   case object ProofOfReturnRefundGiven extends BasisOfClaim
+  case object EvidenceThatGoodsHaveNotEnteredTheEU extends BasisOfClaim
+  case object IncorrectExciseValue extends BasisOfClaim
+  case object CorrectionToRiskClassification extends BasisOfClaim
 
   def basisOfClaimToString(basisForClaim: BasisOfClaim): String = basisForClaim match {
     case DuplicateEntry                         => "Duplicate Entry"
@@ -52,6 +55,9 @@ object BasisOfClaim {
     case Preference                             => "Preference"
     case RGR                                    => "RGR"
     case ProofOfReturnRefundGiven               => "Proof of Return/Refund Given"
+    case EvidenceThatGoodsHaveNotEnteredTheEU   => "Evidence That Goods Have Not Entered The EU"
+    case IncorrectExciseValue                   => "Incorrect Excise Value"
+    case CorrectionToRiskClassification         => "Risk Classification Error"
   }
 
   implicit val format: OFormat[BasisOfClaim] = derived.oformat[BasisOfClaim]()
