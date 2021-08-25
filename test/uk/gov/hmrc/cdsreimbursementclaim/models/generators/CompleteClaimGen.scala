ab3c563ac5380c390261a23910d25222f7ffb51d
Gabor Dorcsinecz <gabor.dorcsinecz@gmail.com>
2021-08-25 16:16:02 +0100

    CDSR-868 Refactored EntryNumber Contact Details Data Model

--- test/uk/gov/hmrc/cdsreimbursementclaim/models/generators/CompleteClaimGen.scala
+++ test/uk/gov/hmrc/cdsreimbursementclaim/models/generators/CompleteClaimGen.scala
@@ -29,8 +29,8 @@ object CompleteClaimGen extends GenUtils {
 
   implicit val detailsRegisteredWithCdsAnswerGen: Gen[DetailsRegisteredWithCdsAnswer]     =
     gen[DetailsRegisteredWithCdsAnswer]
-  implicit val claimantDetailsAsImporterCompanyGen: Gen[ContactDetailsFormData]           =
-    gen[ContactDetailsFormData]
+  implicit val claimantDetailsAsImporterCompanyGen: Gen[ContactDetailsAnswer]             =
+    gen[ContactDetailsAnswer]
   implicit val claimGen: Gen[Claim]                                                       = gen[Claim]
   implicit val claimsAnswerGen: Gen[ClaimsAnswer]                                         = gen[ClaimsAnswer]
   implicit val bankAccountDetailsAnswerGen: Gen[BankAccountDetails]                       =
