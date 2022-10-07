242b5ddac52f0606671e9d810e94c67c6aef2d00
Artur Opala <250927+arturopala@users.noreply.github.com>
2022-10-07 14:37:54 +0100

    [CDSR-2010] change TPI04 endpoint path to be /tpi/getduplicateclaim/v1

--- test/uk/gov/hmrc/cdsreimbursementclaim/connectors/ExistingDeclarationConnectorSpec.scala
+++ test/uk/gov/hmrc/cdsreimbursementclaim/connectors/ExistingDeclarationConnectorSpec.scala
@@ -70,7 +70,7 @@ class ExistingDeclarationConnectorSpec
   )
 
   "Existing Declaration Connector" should {
-    val backEndUrl = s"$baseUrl/prsup/PRRestService/CDFPAY/v1/GetExistingClaim"
+    val backEndUrl = s"$baseUrl/tpi/getduplicateclaim/v1"
 
     (mockConfig.getString(_: String)).expects(*).returning("test-token")
     mockBaseUrl
