e20c30f97ebf94934ff1372e4e1013d5b6839fa5
Haseem Isaac <haseem@tasheel.technology>
2022-10-25 10:07:07 +0100

    CDSR-2026: Fix Telephone number not being sent to TPI05

--- app/uk/gov/hmrc/cdsreimbursementclaim/models/eis/claim/ContactInformation.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaim/models/eis/claim/ContactInformation.scala
@@ -83,7 +83,7 @@ object ContactInformation {
       city = None,
       countryCode = Some(declarantDetails.establishmentAddress.countryCode),
       postalCode = declarantDetails.establishmentAddress.postalCode,
-      telephoneNumber = declarantDetails.contactDetails.map(_.telephone.getOrElse(None)),
+      telephoneNumber = declarantDetails.contactDetails.map(_.telephone.getOrElse(Option.empty)),
       faxNumber = None,
       emailAddress = declarantDetails.contactDetails.flatMap(_.emailAddress)
     )
@@ -98,7 +98,7 @@ object ContactInformation {
       city = None,
       countryCode = Some(consigneeDetails.establishmentAddress.countryCode),
       postalCode = consigneeDetails.establishmentAddress.postalCode,
-      telephoneNumber = consigneeDetails.contactDetails.map(_.telephone.getOrElse(None)),
+      telephoneNumber = consigneeDetails.contactDetails.map(_.telephone.getOrElse(Option.empty)),
       faxNumber = None,
       emailAddress = consigneeDetails.contactDetails.flatMap(_.emailAddress)
     )
