da1dce06bb174e942c2bcd034bd37c4429455d41
MadStu <737342+MadStu@users.noreply.github.com>
2023-05-24 18:17:01 +0100

    Fix bobby violation

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,18 +17,18 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"                %% "bootstrap-test-play-28"     % bootstrapVersion   % Test,
-    "com.typesafe.play"          %% "play-test"                  % current   % Test,
-    "org.scalatest"              %% "scalatest"                  % "3.2.15"   % Test,
-    "org.scalamock"              %% "scalamock"                  % "5.2.0"   % Test,
-    "org.scalatestplus"          %% "scalacheck-1-14"            % "3.2.2.0" % Test,
-    "com.github.alexarchambault" %% "scalacheck-shapeless_1.16"  % "1.3.1"   % Test,
-    "org.scalatestplus.play"     %% "scalatestplus-play"         % "5.1.0"   % Test,
-    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-test-play-28"    % "0.71.0"  % Test,
-    "com.github.chocpanda"       %% "scalacheck-magnolia"        % "0.6.0"   % Test,
-    "com.vladsch.flexmark"        % "flexmark-all"               % "0.64.0"  % Test,
-    "org.pegdown"                 % "pegdown"                    % "1.6.0"   % Test,
-    "com.typesafe.akka"          %% "akka-testkit"               % "2.8.0"  % Test,
-    "org.scala-lang.modules"     %% "scala-xml"                  % "2.1.0"   % Test,
-    "com.eclipsesource"          %% "play-json-schema-validator" % "0.9.5"   % Test
+    "com.typesafe.play"          %% "play-test"                  % current            % Test,
+    "org.scalatest"              %% "scalatest"                  % "3.2.15"           % Test,
+    "org.scalamock"              %% "scalamock"                  % "5.2.0"            % Test,
+    "org.scalatestplus"          %% "scalacheck-1-14"            % "3.2.2.0"          % Test,
+    "com.github.alexarchambault" %% "scalacheck-shapeless_1.16"  % "1.3.1"            % Test,
+    "org.scalatestplus.play"     %% "scalatestplus-play"         % "5.1.0"            % Test,
+    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-test-play-28"    % "0.71.0"           % Test,
+    "com.github.chocpanda"       %% "scalacheck-magnolia"        % "0.6.0"            % Test,
+    "com.vladsch.flexmark"        % "flexmark-all"               % "0.64.0"           % Test,
+    "org.pegdown"                 % "pegdown"                    % "1.6.0"            % Test,
+    "com.typesafe.akka"          %% "akka-testkit"               % "2.6.20"           % Test,
+    "org.scala-lang.modules"     %% "scala-xml"                  % "2.1.0"            % Test,
+    "com.eclipsesource"          %% "play-json-schema-validator" % "0.9.5"            % Test
   )
 }
