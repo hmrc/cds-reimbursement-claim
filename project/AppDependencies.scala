66c53aeb3513bedf51af58c35f52fe8d8eefc7c4
Artur Opala <250927+arturopala@users.noreply.github.com>
2025-04-01 16:43:23 +0100

    [TechDebt] Spring'25 dependecies upgrade (#277)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.6.0"
+  val bootstrapVersion = "9.11.0"
 
   val compile = Seq(
     "uk.gov.hmrc"          %% "bootstrap-backend-play-30"         % bootstrapVersion,
-    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-work-item-repo-play-30" % "2.3.0",
-    "org.typelevel"        %% "cats-core"                         % "2.12.0",
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-work-item-repo-play-30" % "2.6.0",
+    "org.typelevel"        %% "cats-core"                         % "2.13.0",
     "io.github.arturopala" %% "phobos-core"                       % "0.22.0"
   )
 
@@ -19,7 +19,7 @@ object AppDependencies {
     "org.scalamock"          %% "scalamock"                  % "6.0.0"          % Test,
     "org.scalatestplus"      %% "scalacheck-1-18"            % "3.2.19.0"       % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.1"          % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"    % "2.3.0"          % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"    % "2.6.0"          % Test,
     "com.vladsch.flexmark"    % "flexmark-all"               % "0.64.8"         % Test,
     "org.pegdown"             % "pegdown"                    % "1.6.0"          % Test,
     "org.apache.pekko"       %% "pekko-testkit"              % "1.0.3"          % Test,
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -7,7 +7,7 @@ resolvers += Resolver.typesafeRepo("releases")
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 
 addSbtPlugin("uk.gov.hmrc"       % "sbt-auto-build"     % "3.24.0")
-addSbtPlugin("uk.gov.hmrc"       % "sbt-distributables" % "2.5.0")
-addSbtPlugin("org.playframework" % "sbt-plugin"         % "3.0.6")
-addSbtPlugin("org.scalameta"     % "sbt-scalafmt"       % "2.5.2")
-addSbtPlugin("org.scoverage"     % "sbt-scoverage"      % "2.2.2")
+addSbtPlugin("uk.gov.hmrc"       % "sbt-distributables" % "2.6.0")
+addSbtPlugin("org.playframework" % "sbt-plugin"         % "3.0.7")
+addSbtPlugin("org.scalameta"     % "sbt-scalafmt"       % "2.5.4")
+addSbtPlugin("org.scoverage"     % "sbt-scoverage"      % "2.3.1")
