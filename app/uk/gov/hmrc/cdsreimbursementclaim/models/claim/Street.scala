2258dec97bbdfd7da563402d3a01e38d5ff030b5
Artur Opala <250927+arturopala@users.noreply.github.com>
2025-09-04 10:58:10 +0100

    [bugfix] always trim street length to 70

--- app/uk/gov/hmrc/cdsreimbursementclaim/models/claim/Street.scala
+++ app/uk/gov/hmrc/cdsreimbursementclaim/models/claim/Street.scala
@@ -19,16 +19,18 @@ package uk.gov.hmrc.cdsreimbursementclaim.models.claim
 object Street {
 
   def fromLines(line1: Option[String], line2: Option[String]): Option[String] =
-    (line1, line2) match {
-      case (Some(s1), Some(s2)) if s1.trim().endsWith(s2.trim)   => Some(s1)
-      case (Some(s1), Some(s2)) if s2.trim().startsWith(s1.trim) => Some(s2)
-      case (Some(s1), Some(s2))                                  =>
-        if (s1.length() + s2.length() <= 69) Some(s"$s1 $s2")
-        else Some(s"$s1$s2".take(70))
-      case (Some(s1), None)                                      => Some(s1)
-      case (None, Some(s2))                                      => Some(s2)
-      case _                                                     => Some("")
-    }
+    (line1, line2)
+      .match {
+        case (Some(s1), Some(s2)) if s1.trim().endsWith(s2.trim)   => Some(s1)
+        case (Some(s1), Some(s2)) if s2.trim().startsWith(s1.trim) => Some(s2)
+        case (Some(s1), Some(s2))                                  =>
+          if (s1.length() + s2.length() <= 69) Some(s"$s1 $s2")
+          else Some(s"$s1$s2")
+        case (Some(s1), None)                                      => Some(s1)
+        case (None, Some(s2))                                      => Some(s2)
+        case _                                                     => Some("")
+      }
+      .map(_.take(70))
 
   def line1: (Option[String], Option[String]) => Option[String] = {
     case (Some(s1), Some(s2)) if s1.trim().endsWith(s2.trim) =>
