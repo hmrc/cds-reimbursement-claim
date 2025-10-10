ed3f7ff72a9ae1a92c9a0f147de4ed8af5a78bc3
Artur Opala <250927+arturopala@users.noreply.github.com>
2025-10-10 06:36:20 +0100

    [CDSR-4235] upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.1.0"
+  val bootstrapVersion = "10.2.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"         % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-work-item-repo-play-30" % "2.7.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-work-item-repo-play-30" % "2.10.0",
     "org.typelevel"     %% "cats-core"                         % "2.13.0"
   )
 
@@ -18,10 +18,10 @@ object AppDependencies {
     "org.scalamock"          %% "scalamock"                  % "6.0.0"          % Test,
     "org.scalatestplus"      %% "scalacheck-1-18"            % "3.2.19.0"       % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.2"          % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"    % "2.7.0"          % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"    % "2.10.0"         % Test,
     "com.vladsch.flexmark"    % "flexmark-all"               % "0.64.8"         % Test,
     "org.apache.pekko"       %% "pekko-testkit"              % "1.0.3"          % Test,
-    "org.scala-lang.modules" %% "scala-xml"                  % "2.3.0"          % Test,
+    "org.scala-lang.modules" %% "scala-xml"                  % "2.4.0"          % Test,
     "uk.gov.hmrc"            %% "play-json-schema-validator" % "0.1.0"          % Test
   )
 }
