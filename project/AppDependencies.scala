0d2bf64829fe70cdd4d0802d1ea53306c07b8092
rameshuk <ramesh.sukka@gmail.com>
2026-04-07 10:56:44 +0100

    DCWL-4431: fix vulnerabilities issues

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,11 @@ import sbt._
 object AppDependencies {
 
   val playVersion = "-play-30"
-  val bootstrapVersion = "10.6.0"
+  val bootstrapVersion = "10.7.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-backend$playVersion"         % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-work-item-repo$playVersion" % "2.10.0",
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-work-item-repo$playVersion" % "2.12.0",
     "org.typelevel"     %% "cats-core"                              % "2.13.0"
   )
 
@@ -19,7 +19,7 @@ object AppDependencies {
     "org.scalamock"          %% "scalamock"                       % "6.0.0",
     "org.scalatestplus"      %% "scalacheck-1-18"                 % "3.2.19.0",
     "org.scalatestplus.play" %% "scalatestplus-play"              % "7.0.2",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"         % "2.10.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"         % "2.12.0",
     "com.vladsch.flexmark"    % "flexmark-all"                    % "0.64.8",
     "org.apache.pekko"       %% "pekko-testkit"                   % "1.0.3",
     "org.scala-lang.modules" %% "scala-xml"                       % "2.4.0",
